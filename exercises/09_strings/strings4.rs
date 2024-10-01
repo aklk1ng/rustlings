ce40f7a2ecd4ac5b607deeb17b88a643b274bfe3
aklk1ng <2628084745@qq.com>
2024-10-01 10:00:18 +0800

    finish

---
+++ exercises/09_strings/strings4.rs
@@ -0,0 +1,39 @@
+// Calls of this function should be replaced with calls of `string_slice` or `string`.
+fn placeholder(arg: String) {
+    string(arg);
+}
+
+fn string_slice(arg: &str) {
+    println!("{arg}");
+}
+
+fn string(arg: String) {
+    println!("{arg}");
+}
+
+// TODO: Here are a bunch of values - some are `String`, some are `&str`.
+// Your task is to replace `placeholder(…)` with either `string_slice(…)`
+// or `string(…)` depending on what you think each value is.
+fn main() {
+    placeholder("blue".to_string());
+
+    placeholder("red".to_string());
+
+    placeholder(String::from("hi"));
+
+    placeholder("rust is fun!".to_owned());
+
+    placeholder("nice weather".into());
+
+    placeholder(format!("Interpolation {}", "Station"));
+
+    // WARNING: This is byte indexing, not character indexing.
+    // Character indexing can be done using `s.chars().nth(INDEX)`.
+    placeholder(String::from("abc")[0..1].to_string());
+
+    placeholder("  hello there ".trim().to_string());
+
+    placeholder("Happy Monday!".replace("Mon", "Tues"));
+
+    placeholder("mY sHiFt KeY iS sTiCkY".to_lowercase());
+}
